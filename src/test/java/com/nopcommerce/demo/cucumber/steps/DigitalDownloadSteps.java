2025912fe04cb8b79978291828f83ca6b7a7d549
HiteshVagQa <139179850+HiteshVagQa@users.noreply.github.com>
2023-08-20 21:33:43 +0100

    shoppingcart

---
+++ src/test/java/com/nopcommerce/demo/cucumber/steps/DigitalDownloadSteps.java
@@ -0,0 +1,23 @@
+package com.nopcommerce.demo.cucumber.steps;
+
+import com.nopcommerce.demo.pages.DigitalDownloadPage;
+import com.nopcommerce.demo.pages.HomePage;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.testng.Assert;
+
+public class DigitalDownloadSteps {
+
+
+    @Then("^I should navigate to Digital Download successfully$")
+    public void iShouldNavigateToDigitalDownloadSuccessfully() {
+        String expectedMessage = "Digital downloads ";
+        String actualMessage = new DigitalDownloadPage().getDigitalDownloadText();
+        Assert.assertEquals(actualMessage, expectedMessage, "Digital Download page not displayed");
+    }
+
+    @When("^I click on Digital Download link$")
+    public void iClickOnDigitalDownloadLink() {
+        new HomePage().clickOnDigitalDownloadMenu();
+    }
+}
